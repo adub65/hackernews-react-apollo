7721605ae48e407e4ca9ba0c39a37a463e1140bf
Aaron Wilson <adub65@gmail.com>
2020-08-21 14:12:17 -0400

    finish implementing mutation to include redirect to root route

--- src/components/CreateLink.js
+++ src/components/CreateLink.js
@@ -40,7 +40,10 @@ class CreateLink extends Component {
           />
         </div>
 
-        <Mutation mutation={POST_MUTATION} variables={{ description, url }}>
+        <Mutation
+          mutation={POST_MUTATION}
+          variables={{ description, url }}
+          onCompleted={() => this.props.history.push('/')}>
           {postMutation => <button onClick={postMutation}>Submit</button>}
         </Mutation>
       </div>
